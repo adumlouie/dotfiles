eab558c90b83fe16629196c4c799b09d294d4954
adumlouie <adumyumyum@csu.fullerton.edu>
2024-09-16 11:50:44 -0700

    more

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,26 @@
+if status is-interactive
+    function fish_greeting
+        pokemon-colorscripts -r --no-title
+    end
+end
+
+alias nv=nvim
+
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+fzf --fish | source
+starship init fish | source
+
+export FZF_DEFAULT_COMMAND="fd . $HOME"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd -t d . $HOME"
+
+set -gx NVM_DIR ~/.nvm
+set -gx XDG_CONFIG_HOME ~/.config
+source ~/.config/fish/conf.d/nvm.fish
+
+bind \c\[ clear
+
+bind \cz 'fg 2>/dev/null; commandline -f repaint'
+
+fish_add_path /home/snoopanchu/.spicetify
---
+++ dot_config/private_fish/functions/chconf.fish
@@ -0,0 +1,4 @@
+function chconf --wraps='cd ~/.config' --description 'alias chconf=cd ~/.config'
+  cd ~/.config $argv
+        
+end
